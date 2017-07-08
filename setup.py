f1386e615aa287a7fcfdf3fd944516f7d7c0dfd7
George Marshall <geo.marshall@gmail.com>
2017-07-07 19:28:46 -0600

    Removed invalid classifier after twine upload failure

--- setup.py
+++ setup.py
@@ -29,8 +29,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.6'
     ],
     keywords='aws lambda python',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
